474af7715b40b2b3a377fbd46dfc6994bd469b5a
Leandre <leandre.blanchard@epitech.eu>
2019-01-05 17:25:55 +0100

    use of sys/cdefs.h for __THROW and __nonnull macros

--- include/internal.h
+++ include/internal.h
@@ -1,13 +1,13 @@
 #ifndef _LLIST_INTERNAL_H_
 # define _LLIST_INTERNAL_H_
 
-#include <stdio.h>
+# include <stdio.h>
 
-#include "llist.h"
+# include "llist.h"
 
-lnode_t *llist_pop_unused_node(llist_t *list);
-void llist_display(llist_t *list, void (* displayer)(void *data));
-void llist_move_to_unused_node(llist_t *list, lnode_t *node);
-lnode_t *llist_get_node_at(llist_t *list, size_t position);
+lnode_t *llist_pop_unused_node(llist_t *list) __THROW __nonnull((1));
+void llist_display(llist_t *list, void (* displayer)(void *data)) __THROW __nonnull((1, 2));
+void llist_move_to_unused_node(llist_t *list, lnode_t *node) __THROW __nonnull((1, 2));
+lnode_t *llist_get_node_at(llist_t *list, size_t position) __THROW __nonnull((1));
 
 #endif /* !_LLIST_INTERNAL_H_ */
--- include/llist.h
+++ include/llist.h
@@ -1,9 +1,10 @@
 #ifndef _LLIST_H_
 # define _LLIST_H_
 
-#include <stddef.h>
+# include <stddef.h>
+# include <sys/cdefs.h>
 
-#define REALLOC_POOL (5)
+# define REALLOC_POOL (5)
 
 typedef struct lnode_s {
 	void *data;
@@ -20,26 +21,26 @@ typedef struct llinked_list_s {
 	void (* destructor)(void *);
 } llist_t;
 
-int llist_create(llist_t *list, size_t default_len, void (* default_destructor)(void *));
-int llist_reserve(llist_t *list, size_t reserve_size);
-int llist_shrink_to_fit(llist_t *list);
-void llist_destroy(llist_t *list);
+int llist_create(llist_t *list, size_t default_len, void (* default_destructor)(void *)) __THROW __nonnull((1));
+int llist_reserve(llist_t *list, size_t reserve_size) __THROW __nonnull((1));
+int llist_shrink_to_fit(llist_t *list) __THROW __nonnull((1));
+void llist_destroy(llist_t *list) __THROW __nonnull((1));
 
-void llist_clear(llist_t *list);
-void llist_erase(llist_t *list, void *data_to_delete);
-void llist_remove(llist_t *list, lnode_t *node);
+void llist_clear(llist_t *list) __THROW __nonnull((1));
+void llist_erase(llist_t *list, void *data_to_delete) __THROW __nonnull((1));
+void llist_remove(llist_t *list, lnode_t *node) __THROW __nonnull((1, 2));
 
-int llist_insert(llist_t *list, void *new_item, size_t position);
-void llist_erase_at(llist_t *list, size_t position);
+int llist_insert(llist_t *list, void *new_item, size_t position) __THROW __nonnull((1));
+void llist_erase_at(llist_t *list, size_t position) __THROW __nonnull((1));
 
-void *llist_pop_back(llist_t *list);
-void *llist_pop_front(llist_t *list);
+void *llist_pop_back(llist_t *list) __THROW __nonnull((1));
+void *llist_pop_front(llist_t *list) __THROW __nonnull((1));
 
-int llist_push_back(llist_t *list, void *new_item);
-int llist_push_front(llist_t *list, void *new_item);
+int llist_push_back(llist_t *list, void *new_item) __THROW __nonnull((1));
+int llist_push_front(llist_t *list, void *new_item) __THROW __nonnull((1));
 
-void llist_swap(llist_t *l1, llist_t *l2);
+void llist_swap(llist_t *l1, llist_t *l2) __THROW __nonnull((1, 2));
 
-void *llist_get(llist_t *list, size_t position); /* TODO */
+void *llist_get(llist_t *list, size_t position) __THROW __nonnull((1)); /* TODO */
 
 #endif /* !_LLIST_H_ */
